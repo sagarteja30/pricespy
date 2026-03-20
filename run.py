e7ff6e75ffaed4150d32543ee730a6e68bbfc359
CHERUKURI Sagar Teja <SCI50@dsone.3ds.com>
2026-03-20 17:15:19 +0530

    Remove Procfile, fix run.py

--- run.py
+++ run.py
@@ -3,4 +3,9 @@ import uvicorn
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("backend.main:app", host="0.0.0.0", port=port)
+    uvicorn.run(
+        "backend.main:app",
+        host="0.0.0.0",
+        port=port,
+        log_level="info"
+    )
--- start.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-python -c "import uvicorn; uvicorn.run('backend.main:app', host='0.0.0.0', port=int(os.environ.get('PORT', 8000)))"
